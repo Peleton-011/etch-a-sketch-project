1d840b9056d4df9ebb4ce39faa200176655a60b7
Nico <nicodiazan@gmail.com>
2022-06-11 00:09:31 +0200

    Add reset button and minor style changes

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const grid = document.querySelector(".grid")
+const grid = document.querySelector(".grid");
+const r = document.querySelector(":root");
 
-let color;
+let defaultColor = getComputedStyle(r).getPropertyValue("--default-color");
+
+let color = "black";
 let mode = "mouseover";
+
+let side = getComputedStyle(r).getPropertyValue("--grid-side");
 
 function setup () {
     const modeBtn = document.querySelector(".mode");
@@ -9,9 +14,17 @@ function setup () {
         mode = (mode == "mouseover") ? "click" : "mouseover";
         modeBtn.textContent = "Mode: " + mode.replace(mode[0], mode[0].toUpperCase());
     });
+    
+    const resetBtn = document.querySelector(".reset");
+    resetBtn.addEventListener("click", () => {
+        for(let i = 0; i < side * side; i++) {
+           let children = grid.childNodes;
+           children[i].style.backgroundColor = defaultColor;
+        }        
+    });
 }
 
-function populateGrid(side) {
+function populateGrid() {
     //Empty out the current grid
     grid.childNodes.forEach(cell => {
         grid.removeChild(cell);        
@@ -21,7 +34,22 @@ function populateGrid(side) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     for(let i = 0; i < (side * side); i++) {
-        grid.appendChild(cell.cloneNode(true));
+        const newCell = cell.cloneNode(true);
+        switch (i) {
+            case 0:
+                newCell.classList.add("top-left-corner");
+                break;
+            case (side - 1):
+                newCell.classList.add("top-right-corner");
+                break;
+            case (side * (side - 1)):
+                newCell.classList.add("bot-left-corner");
+                break;
+            case (side * side -1):
+                newCell.classList.add("bot-right-corner");
+                break;
+        }
+        grid.appendChild(newCell);
         grid.lastChild.addEventListener("click", (e) => {
             if (mode == "click") {
                 paint(e);
@@ -37,8 +65,8 @@ function populateGrid(side) {
 }
 
 function paint(e) {
-    e.target.style.backgroundColor = "black";
+    e.target.style.backgroundColor = color;
 }
 
 setup();
-populateGrid(4);
+populateGrid();
