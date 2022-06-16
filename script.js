41844de915fc9dd78337b27ab65777d712ee5179
Nico <nicodiazan@gmail.com>
2022-06-16 10:06:25 +0200

    Add color mixing (opacity)
    to achieve that, the rgb value of the paint and the element to be
    painted are turned to ryb to improve the feel of the mix. Then they are
    mixed (pondered average) and finally turned to rgb again.

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const grid = document.querySelector(".grid");
 let defaultColor = getComputedStyle(r).getPropertyValue("--default-color");
 
 let color = getComputedStyle(r).getPropertyValue("--default-paint");
+let ryb;
+let opacityMode = false;
+let opacity = 50;
 let mode = "mouseover";
 let notProMode = true;
 
@@ -12,41 +15,49 @@ let side = getComputedStyle(r).getPropertyValue("--grid-side");
 //Keep track of the state of the mouse (Whether it's being clicked or not)
 var mouseDown = 0;
 
-document.body.onmousedown = () => { 
-  mouseDown = 1;
-}
+document.body.onmousedown = () => {
+    mouseDown = 1;
+};
 document.body.onmouseup = () => {
-  mouseDown = 0;
-}
+    mouseDown = 0;
+};
 
 //Sets up events and creates initial grid
-function setup () {
+function setup() {
     populateGrid();
-    document.querySelectorAll(".pro_tools").forEach(elem => {
+    reset();
+    changeColor(color);
+
+    const opacityBtn = document.querySelector(".opacityMode")
+    opacityBtn.addEventListener("click", (e) => {
+        opacityBtn.classList.toggle("inactive");
+        opacityMode = !opacityMode;
+    });
+
+    document.querySelectorAll(".pro_tools").forEach((elem) => {
         updateHidden(elem);
     });
 
     //Color picker reader
     const colorSelector = document.querySelector(".color_selector");
-    colorSelector.addEventListener("change", e => {
+    colorSelector.addEventListener("change", (e) => {
         color = e.target.value;
+        changeColor(color)
     });
 
     //Pro utils mode button
     const proModeBtn = document.querySelector(".pro_button");
     proModeBtn.addEventListener("click", () => {
-        
         notProMode = !notProMode;
 
-        document.querySelectorAll(".pro_tools").forEach(elem => {
+        document.querySelectorAll(".pro_tools").forEach((elem) => {
             updateHidden(elem);
         });
-        
     });
 
     //Color picker buttons
     const colorBtns = document.querySelectorAll(".color");
-    colorBtns.forEach(btn => {
+    colorBtns.forEach((btn) => {
         btn.addEventListener("click", (e) => {
             color = window.getComputedStyle(e.target).backgroundColor;
         });
@@ -55,18 +66,14 @@ function setup () {
     //Mode button, either mouseover or click
     const modeBtn = document.querySelector(".mode");
     modeBtn.addEventListener("click", () => {
-        mode = (mode == "mouseover") ? "click" : "mouseover";
-        modeBtn.textContent = "Mode: " + mode.replace(mode[0], mode[0].toUpperCase());
+        mode = mode == "mouseover" ? "click" : "mouseover";
+        modeBtn.textContent =
+            "Mode: " + mode.replace(mode[0], mode[0].toUpperCase());
     });
-    
+
     //Reset button, paints all cells in the grid in the default color
     const resetBtn = document.querySelector(".reset");
-    resetBtn.addEventListener("click", () => {
-        for(let i = 0; i < side * side; i++) {
-           let children = grid.childNodes;
-           children[i].style.backgroundColor = defaultColor;
-        }        
-    });
+    resetBtn.addEventListener("click", reset);
 
     //New grid button, generates a new grid of the desired size, from 2x2 to 100x100
     const newGridBtn = document.querySelector(".new");
@@ -76,30 +83,48 @@ function setup () {
             alert("That's too big :c");
             side = 100;
         } else if (side < 2) {
-            alert("That's too smol :c")
+            alert("That's too smol :c");
             side = 2;
-        } 
+        }
         r.style.setProperty("--grid-side", String(side));
         populateGrid();
     });
 
     //Detect when the mouse leaves the screen
-    document.addEventListener("mouseleave", function(event){
-        if(event.clientY <= 0 || event.clientX <= 0 || (event.clientX >= window.innerWidth || event.clientY >= window.innerHeight))
-        {
-           mouseDown = 0;
+    document.addEventListener("mouseleave", function (event) {
+        if (
+            event.clientY <= 0 ||
+            event.clientX <= 0 ||
+            event.clientX >= window.innerWidth ||
+            event.clientY >= window.innerHeight
+        ) {
+            mouseDown = 0;
         }
-      });
+    });
 }
 
 //Hides or unhides all elements below the one you pass
 function updateHidden(elem) {
-    elem.childNodes.forEach(child => {
+    elem.childNodes.forEach((child) => {
         if (child.firstChild) {
             updateHidden(child);
         }
         child.hidden = notProMode;
     });
+}
+
+//Reset all cells to default color
+function reset() {
+    for (let i = 0; i < side * side; i++) {
+        let children = grid.childNodes;
+        children[i].style.backgroundColor = defaultColor;
+    }
+}
+
+//Sets everything up for mixing of colors
+function changeColor(color) {
+    const rgb = strToRgb(color);
+    ryb = rgbToRyb(rgb[0], rgb[1], rgb[2]);
 }
 
 //Creates a grid of any size
@@ -112,19 +137,19 @@ function populateGrid() {
     //Create new grid
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    for(let i = 0; i < (side * side); i++) {
+    for (let i = 0; i < side * side; i++) {
         const newCell = cell.cloneNode(true);
         switch (i) {
             case 0:
                 newCell.classList.add("top-left-corner");
                 break;
-            case (side - 1):
+            case side - 1:
                 newCell.classList.add("top-right-corner");
                 break;
-            case (side * (side - 1)):
+            case side * (side - 1):
                 newCell.classList.add("bot-left-corner");
                 break;
-            case (side * side -1):
+            case side * side - 1:
                 newCell.classList.add("bot-right-corner");
                 break;
         }
@@ -134,27 +159,191 @@ function populateGrid() {
             }
         });
         newCell.addEventListener("mouseover", (e) => {
-            if ((mode != "click")||(mouseDown)) {
+            if (mode != "click" || mouseDown) {
                 paint(e);
             }
         });
 
-        newCell.addEventListener('dragstart', (e) => {
-            e.preventDefault()
-          });
-          
-        newCell.addEventListener('drop', (e) => {
-            e.preventDefault()
-          });
+        newCell.addEventListener("dragstart", (e) => {
+            e.preventDefault();
+        });
+
+        newCell.addEventListener("drop", (e) => {
+            e.preventDefault();
+        });
 
         grid.appendChild(newCell);
     }
-
 }
 
 //Paints element "e" (Changes background color)
 function paint(e) {
-    e.target.style.backgroundColor = color;
+
+    if (!opacityMode) {
+        e.target.style.backgroundColor = color;
+    } else {
+        mix(e);
+    }
+}
+
+// Convert red-green-blue to red-yellow-blue system.
+function rgbToRyb(r, g, b) {
+
+    // Remove the whiteness from the color.
+    let w = Math.min(r, g, b);
+    r = r - w;
+    g = g - w;
+    b = b - w;
+
+    let mg = Math.max(r, g, b);
+    // Get the yellow out of the red+green.
+    let y = Math.min(r, g);
+    r -= y;
+    g -= y;
+
+    // If this unfortunate conversion combines blue and green, then cut each in half to preserve the value's maximum range.
+    if (b && g) {
+        b /= 2.0;
+        g /= 2.0;
+    }
+
+    // Redistribute the remaining green.
+    y += g;
+    b += g;
+
+    // Normalize to values.
+    let my = Math.max(r, y, b);
+    if (my) {
+        let n = mg / my;
+        r *= n;
+        y *= n;
+        b *= n;
+    }
+
+    // Add the white back in.
+    r += w;
+    y += w;
+    b += w;
+
+    const ryb = [r.toFixed(0), y.toFixed(0), b.toFixed(0)];
+
+    // And return back the ryb typed accordingly.
+    return ryb;
+}
+
+// Convert red-yellow-blue to red-green-blue system.
+function rybToRgb(r, y, b) {
+
+    // Remove the whiteness from the color.
+    let w = Math.min(r, y, b);
+    r = r - w;
+    y = y - w;
+    b = b - w;
+
+    let my = Math.max(r, y, b);
+
+    // Get the green out of the yellow and blue
+    let g = Math.min(y, b);
+    y -= g;
+    b -= g;
+
+    if (b && g) {
+        b *= 2.0;
+        g *= 2.0;
+    }
+
+    // Redistribute the remaining yellow.
+    r += y;
+    g += y;
+
+    // Normalize to values.
+    let mg = Math.max(r, g, b);
+    if (mg) {
+        let n = my / mg;
+        r *= n;
+        g *= n;
+        b *= n;
+    }
+
+    // Add the white back in.
+    r += w;
+    g += w;
+    b += w;
+
+    const rgb = [r.toFixed(0), g.toFixed(0), b.toFixed(0)];
+    // And return back the ryb typed accordingly.
+    return (rgb);
+}
+
+// Return the complementary color values for a given color.
+function complimentary(r, g, b, limit) {
+    if (!limit) limit = 255;
+    const result = [limit - r, limit - g, limit - b]; 
+    return result;
+}
+
+function strToRgb (str = "#ffffff") {
+    let r;
+    let g;
+    let b;
+    
+    if (str.slice(0,1) == "#") {
+
+        str = str.slice(1);
+
+        r = parseInt(str.slice(0,2), 16);
+        g = parseInt(str.slice(2,4), 16);
+        b = parseInt(str.slice(4), 16);
+
+    } else if (str.slice(0,3) == "rgb") {
+
+        str = str.slice(4);
+        str = str.replace(")", "")
+        const strs = str.split(", ");
+
+        r = Number(strs[0]);
+        g = Number(strs[1]);
+        b = Number(strs[2]);
+    }
+    
+    const rgb = [r, g, b];
+    return rgb;
+}
+
+function rgbToStr (r = 255, g = 255, b = 255) {
+    let color = "#";
+
+    r = Number(r);
+    g = Number(g);
+    b = Number(b);
+    
+    color += r.toString(16).padStart(2, "0");
+    color += g.toString(16).padStart(2, "0");
+    color += b.toString(16).padStart(2, "0");
+
+    return color;
+}
+
+//Paints the cell that triggered e as the mix of paint color and current color of that element
+function mix(e){
+
+    let currColor = e.target.style.backgroundColor;
+    let rgb2 = strToRgb(currColor);
+    const ryb2 = rgbToRyb(rgb2[0], rgb2[1], rgb2[2]);
+    const finalRyb = [];
+
+    for (let i = 0; i < 3; i++) {
+        let proportioned1 = Number(ryb[i]) * (opacity / 100);
+        let proportioned2 = Number(ryb2[i]) * (1 - (opacity / 100));
+        let aux = proportioned1 + proportioned2;
+        finalRyb.push(aux.toFixed(0));    
+    }
+
+    const result = rybToRgb(finalRyb[0], finalRyb[1], finalRyb[2]);
+
+    let resultStr = rgbToStr(result[0], result[1], result[2]);
+
+    e.target.style.backgroundColor = resultStr;
 }
 
 setup();
